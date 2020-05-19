552d5e7e62ae21029354789504bfc82505ae928b
Rob-Swensen <rg.swensen801@gmail.com>
2020-05-19 14:03:26 -0600

    Carousel complete

---
+++ src/Components/Images.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Images({src}){
+    return <img src={src} alt='slide-img' className='slide-image'/>
+}
+
+export default Images
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,17 +1,49 @@
-import React from "react";
-import '../Styles/slider.scss';
+import React, { useState } from "react";
+import "../Styles/slider.scss";
+import Images from "./Images";
+import I1 from "../Pics/Cyberpunk.jpg";
+import I2 from "../Pics/Ghost.jpg";
+import I3 from "../Pics/Rdr2.jpg";
+import I4 from "../Pics/TLOU2.jpg";
+import I5 from "../Pics/WZ.jpg";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
 
 function Slider(props) {
-  let sliderArr = [1, 2, 3, 4, 5];
+  let sliderArr = [
+    <Images src={I1} />,
+    <Images src={I2} />,
+    <Images src={I3} />,
+    <Images src={I4} />,
+    <Images src={I5} />,
+  ];
+  const [x, setX] = useState(0);
+  const goLeft = () => {
+    x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
+  };
+  const goRight = () => {
+    x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
+  };
   return (
     <div className="slider">
       {sliderArr.map((item, index) => {
         return (
-          <div key={index} className="slide">
+          <div
+            key={index}
+            className="slide"
+            style={{ transform: `translateX(${x}%)` }}
+          >
             {item}
           </div>
         );
       })}
+      <button id="goLeft" onClick={goLeft}>
+        <FontAwesomeIcon className='icon' icon={faChevronLeft} size='4x'/>
+      </button>
+      <button id="goRight" onClick={goRight}>
+        <FontAwesomeIcon className='icon' icon={faChevronRight} size='4x'/>
+      </button>
     </div>
   );
 }
